c35895e5bedcaace1ffd69a3fb0473a999b9639a
Kevin Ernst <kevin.ernst@cchmc.org>
2025-03-27 08:55:37 -0400

    (web) Windows template path sep. fix (Jinja issue #711)
    - work around pallets/jinja #711 by subclassing jinja2.FileSystemLoader
      and overriding the `get_source` method so it doesn't balk at
      backslashes in pathnames (on Windows)
    
      see https://github.com/pallets/jinja/discussions/2080 for details
    
    - also add a `reallyclean` target that removes the deploy dir,
      useful when you’re developing locally or on the "dev" server

--- web/lib/templates.py
+++ web/lib/templates.py
@@ -1,93 +1,132 @@
 """
 Makefile helper functions related to site generation and admin
 """
-import os
-import sys
-import logging
+import os, sys, logging
+from datetime import datetime as dt
+from collections.abc import Iterable
+from jinja2 import Environment, FileSystemLoader, pass_context
 
+from .util import filesize
 from .config import use_config
 
+DEFAULT_TEMPLATE_DIR = 'templates'
 log = logging.getLogger(__name__)
 
 if os.getenv('DEBUG') or os.getenv('DEBUG_TEMPLATES'):
     log.setLevel(logging.DEBUG)
 
 
+class Issue711Environment(Environment):
+    """
+    Work around pallets/jinja issue #711
+
+    Jinja "template paths are not necessarily filesystem paths and […] use
+    forward slashes," so just do a crude subsitution and hope for the best
+
+    ref. https://github.com/pallets/jinja/issues/711#issuecomment-300070379
+    """
+    def get_template(self, name, parent=None, globals=None):
+        name = name.replace('\\', '/')
+        return super().get_template(name, parent, globals)
+
+
+class BackslashFriendlyFileSystemLoader(FileSystemLoader):
+    """
+    Work around pallets/jinja issue #711
+
+    Jinja "template paths are not necessarily filesystem paths and […] use
+    forward slashes," so just do a crude subsitution and hope for the best
+
+    ref. https://github.com/pallets/jinja/issues/711#issuecomment-300070379
+    and  https://github.com/pallets/jinja/blob/3.1.6/src/jinja2/loaders.py#L194
+    """
+    def get_source(self, environment, template):
+        template = template.replace('\\', '/')
+        return super().get_source(environment, template)
+
+
 @use_config
 def process_templates(templates=None, deploydir=None, config=None):
     """
     Process Jinja2 templates from ``templates``; write to ``deploydir``
-    """
-    from datetime import datetime as dt
-    from collections.abc import Iterable
-    from jinja2 import Environment, FileSystemLoader, pass_context
-    from .util import filesize
 
+    :param Union(str, list) templates: either 1) a template directory; 2) the
+        name of a single template relative to the default template directory
+        (``templates`` in the c.w.d.); or 3) a list of templates relative to
+        the template directory
+    :param str deploydir: where to write the processed templates
+    :param dict config: parsed config passed in by the :py:deco:`use_config`
+        decorator in :py:mod:`lib.config`
+    """
     if not deploydir:
         deploydir = config['site']['deploydir']
         log.info(f"No 'deploydir' given, using default of '{deploydir}'")
 
+    # use 'templates' in the c.w.d. by default, unless specified
+    loader = BackslashFriendlyFileSystemLoader(DEFAULT_TEMPLATE_DIR)
+    env = Issue711Environment(loader=loader)
+
+    # - default to `templates` subdir in the current working directory
+    # - if passed a custom template directory, iterate over that instead
+    # - if passed a single string, wrap in a list, use default template dir.
     if not templates:
-        # default to `templates` subdir in the current working directory
-        templates = 'templates'
+        templates = DEFAULT_TEMPLATE_DIR
+    elif isinstance(templates, str) and os.path.isdir(templates):
+        env.loader = BackslashFriendlyFileSystemLoader(templates)
     elif isinstance(templates, str) and not os.path.isdir(templates):
-        # wrap in a list
         templates = [templates]
+    else:
+        if not isinstance(templates, Iterable):
+            raise RunTimeError(f"Unexpected value templates='{templates}'")
+
+    # add parsed TOML config to the environment for all templates; so that
+    # `title` from `site.toml` becomes `{{ site.title }}` in Jinja templates
+    env.globals = config
+
+    ##
+    ##  Jinja custom template filters
+    ##  ref. https://jinja.palletsprojects.com/en/stable/api/#writing-filters
+    ##
+    env.filters['filesize'] = lambda p: filesize(p)
 
     if isinstance(templates, str) and os.path.isdir(templates):
         print(f"Searching template directory '{templates}'...",
               file=sys.stderr)
-        env = Environment(loader=FileSystemLoader(templates))
-        # FIXME: duplication
-        env.globals = config
-        env.filters['filesize'] = lambda p: filesize(p)
 
         for (path, dirs, files) in os.walk(templates):
-            # path relative to `templates` directory
             relpath = os.path.relpath(path, templates)
-
-            # corresponding destination path in "deploy" directory
             destpath = os.path.join(deploydir, relpath)
             os.makedirs(destpath, exist_ok=True)
 
             for f in files:
-                if f.endswith('.swp'):  # ugh
+                if f.endswith('.swp'):  # FIXME: other editor swap files
                     continue
 
                 destfile = os.path.join(destpath, f)
-                # the templates are UTF-8, so force this encoding for Windows,
-                # which defaults to cp1252
+                # the templates are UTF-8, so force the output encoding for
+                # Windows, where locale.getencoding() defaults to `cp1252`
                 with open(destfile, 'w', encoding='utf-8') as df:
-                    # https://github.com/pallets/jinja/issues/711#issuecomment-300070379
-                    tpath = os.path.join(relpath, f).replace('\\', '/')
+                    tpath = os.path.join(relpath, f)
                     t = env.get_template(tpath)
-                    df.write(t.render(), )
+                    df.write(t.render())
 
                 print(f"Wrote '{destfile}'.", file=sys.stderr)
 
             for d in dirs:
-                # skip `_partials` and anything else beginning with an underscore
+                # skip `_partials` and anything else with an underscore
                 if d.startswith('_'):
                     dirs.remove(d)
-
     else:
-        assert isinstance(templates, list)
-        env = Environment(loader=FileSystemLoader('.'))
-        # FIXME: duplication
-        env.globals = config
-        # create a `filesize` filter that can be used in templates
-        env.filters['filesize'] = lambda p: filesize(p)
-
+        # it's a list of individual template names
         for template in templates:
             # assumed to be relative to `templates` subdir
             destpath = os.path.join(deploydir, os.path.dirname(template))
             os.makedirs(destpath, exist_ok=True)
             destfile = os.path.join(destpath, os.path.basename(template))
 
-            with open(destfile, 'w') as df:
-                # https://github.com/pallets/jinja/issues/711 again, *sigh*
-                tpath = os.path.join('templates', template).replace('\\', '/')
-                t = env.get_template(tpath)
+            # force output encoding for Windows
+            with open(destfile, 'w', encoding='utf-8') as df:
+                t = env.get_template(template)
                 df.write(t.render())
 
             print(f"Wrote '{destfile}'.", file=sys.stderr)
